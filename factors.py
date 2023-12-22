16f81d1920d1c4087c85c8b1e9404710044f1b9b
jospeh007 <youssefgf123@gmail.com>
2023-12-22 13:32:12 -0800

    factors

--- factors.py
+++ factors.py
@@ -1,32 +1,32 @@
+#!/usr/bin/python3
+# library to get arguments
 import sys
 
-def factorize(n):
-    for i in range(2, n // 2 + 1):
-        if n % i == 0:
-            print(f"{n}={i}*{n//i}")
-            return
 
-    # If no factorization found (prime number)
-    print(f"{n} is a prime number")
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: {} <file>".format(sys.argv[0]))
-        sys.exit(1)
-
-    file_path = sys.argv[1]
-
+# fn unpack number factorial
+def fc():
+    """
+    function fc to search file to convert number and format n=p*q
+    """
     try:
-        with open(file_path, 'r') as file:
-            for line in file:
-                number = int(line.strip())
-                factorize(number)
-    except FileNotFoundError:
-        print(f"Error: File '{file_path}' not found.")
-        sys.exit(1)
-    except ValueError:
-        print(f"Error: Invalid content in file '{file_path}'. All lines must contain valid natural numbers.")
-        sys.exit(1)
+        revfile = sys.argv[1]
+        with open(revfile) as f:
+            for revnumber in f:
+                revnumber = int(revnumber)
+                if revnumber % 2 == 0:
+                        print("{}={}*{}".format(revnumber, revnumber // 2, 2))
+                        continue
+                i = 3
+                while i < revnumber // 2:
+                    if revnumber % i == 0:
+                        print("{}={}*{}".format(revnumber, revnumber // i, i))
+                        break
+                    i = i + 2
+                if i == (revnumber // 2) + 1:
+                    print("{}={}*{}".format(revnumber, revnumber, 1))
+    except (IndexError):
+        pass
 
-if __name__ == "__main__":
-    main()
+
+# autostart
+fc()
